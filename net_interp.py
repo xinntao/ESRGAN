50fbd2de1d80a014e1c0e1c165913a128f5a8384
xinntao <wxt1994@126.com>
2018-09-05 23:28:53 +0800

    fixbug net_interp

--- net_interp.py
+++ net_interp.py
@@ -6,15 +6,16 @@ alpha = float(sys.argv[1])
 
 net_PSNR_path = './models/RRDB_PSNR_x4.pth'
 net_ESRGAN_path = './models/RRDB_ESRGAN_x4.pth'
-net_interp_path = './models/interp_{:02d}.pth'.format(alpha*10)
+net_interp_path = './models/interp_{:02d}.pth'.format(int(alpha*10))
 
 net_PSNR = torch.load(net_PSNR_path)
 net_ESRGAN = torch.load(net_ESRGAN_path)
 net_interp = OrderedDict()
 
-print('Interpolating with alphs = ', alpha)
+print('Interpolating with alpha = ', alpha)
+
 for k, v_PSNR in net_PSNR.items():
     v_ESRGAN = net_ESRGAN[k]
-    net_interp[k] = alpha * v_PSNR + (1 - alpha) * v_ESRGAN
+    net_interp[k] = (1 - alpha) * v_PSNR + alpha * v_ESRGAN
 
-torch.save(net_interp_path, net_interp)
+torch.save(net_interp, net_interp_path)
